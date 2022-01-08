89c74a8434ff4c3be9ef149beb322f6744735f3e
sharnoff <github@max.sharnoff.org>
2022-01-07 23:29:16 -0800

    Add private 'sync.sh' script
    Adding this because I think it's kinda interesting. There's some
    relatively complex stuff going on there in order to properly manage
    configuration changes & such. :shrug:

---
+++ sync.sh
@@ -0,0 +1,124 @@
+#!/bin/sh
+#
+# Private script for synchronizing the local files back to sharnoff.io
+#
+# Optional args:
+#  --bin <name>  Copies the binary linked to by bin/<name> to the server's bin/<name>
+#
+# The --bin argument is typically used with target/release/http-server 
+
+set -e
+
+dir=$(dirname "$0")
+
+print_usage_and_exit () {
+    echo 'Usage: sync.sh [ --bin <name> ]'
+    exit 1
+}
+
+# Usage: <cmd> | indent <string>
+#
+# Prefixes <string> to all lines on STDIN and outputs them. The prefix string must not contain any
+# slashes; this will cause failures
+indent () {
+    sed -e "s/^/$1/g"
+}
+
+INDENT_STR="    " # 4 spaces
+
+if [[ $# -eq 2 ]]; then
+    if [[ "$1" != '--bin' ]]; then
+        print_usage_and_exit
+    fi
+
+    if [[ ! -f "$dir/bin/$2" ]]; then
+        echo "No binary $2 in bin"
+        exit 1
+    fi
+
+    cmd="scp $(realpath "$dir/bin/$2") max@sharnoff.io:~/website/bin/$2"
+
+    echo ":: Synchronizing $2 to sharnoff.io:~/website/bin..."
+    echo ":: > $cmd"
+    $cmd | indent '    '
+    echo ':: Done'
+    exit 0
+elif [[ $# -ne 0 ]]; then
+    print_usage_and_exit
+fi
+
+dry_run_cmd="rsync -avn --exclude-from=.rsync-exclude $dir max@sharnoff.io:~/website"
+actual_cmd="rsync -avz --exclude-from=.rsync-exclude $dir max@sharnoff.io:~/website"
+
+echo ':: Performing dry-run...'
+echo ":: > $dry_run_cmd"
+
+$dry_run_cmd | indent "$INDENT_STR"
+
+while true; do
+    echo -n ':: Confirm? [y/n] '
+
+    read resp
+    case "$resp" in
+        'y')
+            break
+            ;;
+        'n')
+            echo ':: Exiting without synchronizing.'
+            exit 0
+            ;;
+        *)
+            continue
+            ;;
+    esac
+done
+
+echo ':: Performing full synchronization...'
+echo ":: > $actual_cmd"
+
+cmd_output="$($actual_cmd | indent "$INDENT_STR" | tee /dev/tty)"
+
+echo ':: Done'
+
+updates=""
+if grep -q "^${INDENT_STR}content/photos/." <(echo "$cmd_output"); then
+    updates="photos"
+fi
+
+if grep -q "^${INDENT_STR}content/blog-posts/." <(echo "$cmd_output"); then
+    updates="$updates blog"
+fi
+
+if [[ ! -z "$updates" ]]; then
+
+    echo ":: Send update signal '$updates'..."
+    echo ":: > ssh max@sharnoff.io \"echo '$updates' >> website/updated\""
+
+    ssh max@sharnoff.io "echo '$updates' >> website/updated" | indent "$INDENT_STR"
+
+    echo ':: Done'
+fi
+
+if grep -q "^${INDENT_STR}Caddyfile" <(echo "$cmd_output"); then
+    echo -n ':: Caddyfile has been updated. Copy over? [y/n] '
+    while true; do
+        read resp
+        case "$resp" in
+            'y')
+                echo ':: Sending Caddyfile...'
+                echo ":: > ssh max@sharnoff.io 'caddy reload --config=website/Caddyfile'"
+
+                ssh max@sharnoff.io 'caddy reload --config=website/Caddyfile' | indent "$INDENT_STR"
+
+                echo ':: Done'
+                break
+                ;;
+            'n')
+                break
+                ;;
+            *)
+                echo -n ':: Copy over Caddyfile? [y/n] '
+                ;;
+        esac
+    done
+fi
